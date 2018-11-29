87e20ce7c7fba9a597b569bb2aa9464e701ce183
femke77 <megan.meyers.388@my.csun.edu>
2018-11-28 19:03:50 -0800

    made db access ava to any module

--- startup/db.js
+++ startup/db.js
@@ -1,18 +1,33 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const dbName = 'morefun';
-const url = 'mongodb://localhost'
-const client = new MongoClient(url);
 
 
-module.exports = function() {
-  
-    client.connect(function(err) {
-        assert.equal(null, err);
-        console.log(`Successfully connected to database ${dbName}...`);
-        const db =  client.db(dbName);
-        client.close();
-    });
-
-
+let state = {
+    db: null
 }
+
+exports.connect = function(url, done) {
+    if (state.db) return done();
+    
+    MongoClient.connect(url, {useNewUrlParser: true}, function (err, db){
+        if (err) return done(err);
+        state.db = db;
+        done();
+    });
+}
+
+
+exports.get = function() {
+    return state.db;
+}
+
+
+exports.close = function(done) {
+    if (state.db) {
+        state.db.close(function(err, result) {
+            state.db = null;
+            state.mode = null;
+            done(err);
+        });
+    }
+}
+
