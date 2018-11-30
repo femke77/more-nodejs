cc399ce43f6aa39355b68d594836aa7b12ab72f8
femke77 <megan.meyers.388@my.csun.edu>
2018-11-30 02:39:57 -0800

    able to fetch json and input to db

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const app = express();
-const {connect}= require('./startup/db');
+const db = require('./startup/db');
 const url = 'mongodb://localhost/morefun';
 
 
 require('./startup/routes')(app); 
 
 const port = process.env.PORT || 5000;
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
-
-connect(url, function(err) {
+db.connect(url, function(err) {
     if (err){
         console.log('cannot connnect to db');
-        process.exit(1);
+        process.exit(1);                     
     } else {
-        app.listen(port, () => console.log(`Connected to ${url} and express listening on port ${port}`));
+        console.log(`Connected to ${url}`);        
+       
     }
 });
 
-//test:
-//axios.get('https://cat-fact.herokuapp.com/facts').then((result) => console.log(JSON.stringify(result.data, null, 2)));
+
+module.exports = server;
+
+
+
 
--- startup/db.js
+++ startup/db.js
@@ -1,33 +1,65 @@
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb')
+const axios = require('axios');
+const url = 'https://catfact.ninja/fact'
+let db = null;
 
+//get number by doing a call to "total" under/facts and limit is "per page" then use axios all the fill the array and return the array
+//by doing each get but resovling the promises together
 
-let state = {
-    db: null
+let getData = function getData() {
+    let data = axios.get(url)
+    .then(response => {
+        return response.data
+    }).catch(error => console.log(error))
+    return data;
 }
 
-exports.connect = function(url, done) {
-    if (state.db) return done();
+exports.connect = async function(url, done) {
+    if (db) return done();
+    let data = await getData();
     
-    MongoClient.connect(url, {useNewUrlParser: true}, function (err, db){
+ 
+    
+    MongoClient.connect(url, {useNewUrlParser: true}, function (err, client){
         if (err) return done(err);
-        state.db = db;
+        db = client.db('morefun');    
+        db.collection('catfacts').insertMany(dataArray, function(err, res){
+            if (err) throw err;
+            console.log(res.insertedCount);
+        })
         done();
+        
+        
     });
+      
+    
 }
-
 
 exports.get = function() {
-    return state.db;
+    return db;
 }
 
 
 exports.close = function(done) {
-    if (state.db) {
-        state.db.close(function(err, result) {
-            state.db = null;
-            state.mode = null;
+    if (db) {
+        db.close(function(err, result) {
+            db = null;
+            mode = null;
             done(err);
         });
     }
 }
 
+exports.httpRequest = function() {
+        console.log(db)
+        
+      //  axios.get('https://cat-fact.herokuapp.com/facts').then((result) => db.collection('catfacts').insertMany(result, err => console.log(err)))
+  
+    //JSON.stringify(result, 2, null)
+   
+}
+ 
+
+
+ //axios.get('https://cat-fact.herokuapp.com/facts').then((result) => console.log(JSON.stringify(result.data, null, 2)));
+ //(res) => db.collection('catfacts').insertMany(res) 
